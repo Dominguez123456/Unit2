a92fbcc45cdcd98c28eaa6de8dece3ed07ceada0
Stephen <dominguezs@roverkids.org>
2025-04-25 14:16:50 -0400

    PlayDetchDone

--- PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float dogTimeout = 0.5f;
+    public float dogTime = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && dogTime == 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogTime = Time.time;
+        }
+        if (dogTime + dogTimeout < Time.time)
+        {
+            dogTime = 0.0f;
         }
     }
 }
